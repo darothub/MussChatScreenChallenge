8cf4ac524eebacc410c1eff918d556806c17c2f8
Abdulrasaq Durosomo <darotudeen@gmail.com>
2024-03-23 00:45:30 +0000

    Ch project setup #2 (#11)
    * [#2]- setup theme and package hierarchy
    
    ---------
    
    Co-authored-by: Abdulrasaq Durosomo <durosomo.abdulrasaq@gmail.com>

--- app/src/main/java/com/darothub/musschatscreen/MainActivity.kt
+++ app/src/main/java/com/darothub/musschatscreen/MainActivity.kt
@@ -3,13 +3,6 @@ package com.darothub.musschatscreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.darothub.musschatscreen.ui.theme.MussChatScreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,29 +10,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MussChatScreenTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MussChatScreenTheme {
-        Greeting("Android")
-    }
-}
