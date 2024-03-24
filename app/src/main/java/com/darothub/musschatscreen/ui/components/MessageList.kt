95ec24749424816442ef7ab63ac113b578b95d44
Abdulrasaq Durosomo <durosomo.abdulrasaq@gmail.com>
2024-03-24 06:20:47 +0000

    [#6]-added animation to message display

--- app/src/main/java/com/darothub/musschatscreen/ui/components/MessageList.kt
+++ app/src/main/java/com/darothub/musschatscreen/ui/components/MessageList.kt
@@ -1,5 +1,6 @@
 package com.darothub.musschatscreen.ui.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -7,9 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.darothub.musschatscreen.model.Message
 import com.darothub.musschatscreen.ui.main.currentUser
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ColumnScope.MessageList(messages: List<Message>) {
@@ -20,7 +28,20 @@ fun ColumnScope.MessageList(messages: List<Message>) {
             val isMe = message.sender == currentUser
             val alignment = if (isMe) Arrangement.Start else Arrangement.End
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = alignment) {
-                MessageBubble(isMe = isMe, message = message)
+                var animationState by remember { mutableStateOf(false) }
+                LaunchedEffect(Unit) {
+                    launch {
+                        animationState = true
+                    }
+                }
+                AnimatedContent(
+                    targetState = animationState,
+                ) { content ->
+                    if (content){
+                        MessageBubble(isMe = isMe, message = message)
+                    }
+                }
+
             }
         }
     }
