cea82a53d86c9e37fd31c9dd19c5a64d8eb407ee
Abdulrasaq Durosomo <durosomo.abdulrasaq@gmail.com>
2024-03-26 00:12:27 +0000

    [#8]-improved simulated response

--- app/src/main/java/com/darothub/musschatscreen/MainActivity.kt
+++ app/src/main/java/com/darothub/musschatscreen/MainActivity.kt
@@ -16,6 +16,12 @@ import com.darothub.musschatscreen.presentation.viewmodel.ConversationViewModel
 import com.darothub.musschatscreen.presentation.viewmodel.ConversationViewModelFactory
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+val listOfLetters = listOf("a", "b", "w", "c", "o", "e", "i", "u")
+val sentence = listOf(
+    "Try another word with letter ",
+    "hmm you must good at using letter",
+    "teach me how to use letter"
+).random()
 
 class MainActivity : ComponentActivity() {
     private val conversationViewModel by viewModels<ConversationViewModel> {
@@ -23,22 +29,36 @@ class MainActivity : ComponentActivity() {
             ServiceLocator.getInstance().messageRepository
         )
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val scope = rememberCoroutineScope()
             val collectedMessages = conversationViewModel.messages.collectAsStateWithLifecycle()
+            var counter = 0
             MainScreen(messages = collectedMessages) { content ->
                 when {
                     content.isNotEmpty() -> {
                         currentUser.says {
                             conversationViewModel.addMessage(Message(sender = it, content = content))
                         }
+                        val letters = listOfLetters.filter { content.contains(it) }
+                        if (letters.isEmpty()){
+                            return@MainScreen
+                        }
+                        val letter = letters[0]
+                        val otherResponse = "$sentence $letter"
+
                         scope.launch {
                             delay(Number.TWO_SECONDS)
                             Sender.OTHER.says {
-                                conversationViewModel.addMessage(Message(sender = it, content = content))
+                                counter++
+                                if (counter<3){
+                                    conversationViewModel.addMessage(Message(sender = it, content = otherResponse))
+                                }
+                                if (counter in 3..5){
+                                    val message = "I'm waiting for a letter I do not know"
+                                    conversationViewModel.addMessage(Message(sender = it, content = message))
+                                }
                             }
                         }
                     }
